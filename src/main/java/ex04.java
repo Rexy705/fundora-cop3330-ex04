e3a1ebc9c59396d9b30e1605d4b6785360d547e3
Rey <reyfundora@icloud.com>
2021-05-31 22:03:10 -0400

    spacing fix

--- src/main/java/ex04.java
+++ src/main/java/ex04.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class ex04 {
     public static void main (String[] args){
-        Scanner input1 = new Scanner(System.in),
-                input2 = new Scanner(System.in),
-                input3 = new Scanner(System.in),
-                input4 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a noun: ");
-        String noun = input1.next();
+        String noun = input.next();
         System.out.print("Enter a verb: ");
-        String verb = input2.next();
+        String verb = input.next();
         System.out.print("Enter an adjective: ");
-        String adjective = input3.next();
+        String adjective = input.next();
         System.out.print("Enter an adverb: ");
-        String adverb = input4.next();
+        String adverb = input.next();
 
         System.out.println("Do you " + verb + " your " + adjective + " " + noun + " "
                             + adverb + "? That's hilarious!");
